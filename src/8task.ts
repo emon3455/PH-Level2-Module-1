ef17149985f2dc6885f882e52c109c43435da77a
Emon <emon.mhk69@gmail.com>
2025-05-08 00:12:23 +0600

    task 8 complete

---
+++ src/8task.ts
@@ -0,0 +1,16 @@
+{
+  async function squareAsync(n: number): Promise<number> {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (n < 0) {
+          reject(new Error("Negative number not allowed"));
+        } else {
+          resolve(n * n);
+        }
+      }, 1000);
+    });
+  }
+
+  squareAsync(4).then(console.log); // Output after 1s: 16
+  squareAsync(-3).catch(console.error); // Output: Error: Negative number not allowed
+}
